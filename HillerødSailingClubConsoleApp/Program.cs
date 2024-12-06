732d28cf2e277eec520078e019eacb9d8041ab50
OKronborg00 <oliverkronborg00@gmail.com>
2024-12-06 12:14:56 +0100

    change name from Activity to Event class

--- HillerødSailingClubConsoleApp/Program.cs
+++ HillerødSailingClubConsoleApp/Program.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Reflection.Metadata;
 
 Boat boat = new Boat(1, 4, "BodNavn", 2000, "benut", "macedes", 234, "julie");
-Activity activity = new Activity(1, "juleaften", "der holdes juleaften event", 2024, 12, 24);
+Event evented = new Event(1, "juleaften", "der holdes juleaften event", 2024, 12, 24);
 Blog blog = new Blog(1, "blogTitel", "text text text");
 // booking create 
 Employee employee = new Employee("kunde", 1, "casper", 42418990, "test@mail.com", "addresseTest1");
@@ -12,6 +12,7 @@ Member member = new Member(1, "casper", 42418990, "test@mail.com", "addresseTest
 
 
 BoatDict boatDict = new BoatDict();
+
 Console.WriteLine(boat);
-Console.WriteLine(activity);
+Console.WriteLine(evented);
 Console.WriteLine(blog);
