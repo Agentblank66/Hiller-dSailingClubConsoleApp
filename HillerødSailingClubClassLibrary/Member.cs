01519975aece35beeb37f64943ad558c2796e545
AgentBlank66 <watchandlearn626@gmail.com>
2024-12-06 11:56:34 +0100

    Creating a Member class with Constructor and Methods: ToString

---
+++ HillerødSailingClubClassLibrary/Member.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HillerødSailingClubClassLibrary
+{
+    public class Member
+    {
+        #region Property
+        #endregion
+
+        #region Constructor
+        public Member(int id, string name, int tlf, string email, string address) : base(id, name, tlf, email, address)
+        {
+        }
+        #endregion
+
+        #region Methods
+        // This method overrides the base ConsoleWriteLine
+        public override string ToString()
+        {
+            return $"Id: {Id}" + $"\nName: {Name}" + $"\nTlf: {Tlf}" + $"\nEmail: {Email}" + $"\nAddress: {Address}";
+        }
+        #endregion
+    }
+}
