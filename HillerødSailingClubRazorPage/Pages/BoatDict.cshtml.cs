1776f4328f7797908bed63d53e55267a41829210
OKronborg00 <oliverkronborg00@gmail.com>
2024-12-08 13:04:56 +0100

    + Boat page + boats page

---
+++ HillerødSailingClubRazorPage/Pages/BoatDict.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HillerødSailingClubRazorPage.Pages
+{
+    public class BoatDictModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
