6d0c7ee3270a4e1e219a1103a3f50e3a1ce464d5
Rasmus Dyring Raavig <rar002@edu.zealand.dk>
2024-12-11 12:37:16 +0100

    changes of layout of events in program

--- HillerødSailingClubConsoleApp/Program.cs
+++ HillerødSailingClubConsoleApp/Program.cs
@@ -77,44 +77,44 @@ Events event3 = new Events(3, "Sommerstart", "Der fejres at sommeren begynder.",
 EventList events = new EventList();
 Console.WriteLine();
 
-//Add events to Events List and then printing all events
-Console.WriteLine("List of Events:");
-events.AddEvents(event1);
-Console.WriteLine();
-events.AddEvents(event2);
-Console.WriteLine();
-events.AddEvents(event3);
-List<Events> allEvents = events.GetAllEvents();
-foreach(Events events1 in allEvents) Console.WriteLine(events1);
-Console.WriteLine();
+    //Add events to Events List and then printing all events
+    Console.WriteLine("List of Events:");
+    events.AddEvents(event1);
+    Console.WriteLine();
+    events.AddEvents(event2);
+    Console.WriteLine();
+    events.AddEvents(event3);
+    List<Events> allEvents = events.GetAllEvents();
+    foreach(Events events1 in allEvents) Console.WriteLine(events1);
+    Console.WriteLine();
 
-//Update an event and the showing that one event
-Console.WriteLine("Update of Event:");
-events.UpdateEvents(1, "Juleaften", "Der holdes Juleaften.", 2025, 12, 24, 18, 00, 00);
-Console.WriteLine(events.GetEvents(1)); 
-Console.WriteLine();
+    //Update an event and the showing that one event
+    Console.WriteLine("Update of Event:");
+    events.UpdateEvents(1, "Juleaften", "Der holdes Juleaften.", 2025, 12, 24, 18, 00, 00);
+    Console.WriteLine(events.GetEvents(1)); 
+    Console.WriteLine();
 
-//Snak med henrik om search metode, selvom den er blevet lavet, hør hvorfor man skal gøre på denne måde
-Console.WriteLine("Search of Events with, holdes, in the description:");
-List<Events> eventlist = events.SearchEvents("holdes");
-foreach(Events Events in eventlist) Console.WriteLine(Events);
-Console.WriteLine();
+    //Snak med henrik om search metode, selvom den er blevet lavet, hør hvorfor man skal gøre på denne måde
+    Console.WriteLine("Search of Events with, holdes, in the description:");
+    List<Events> eventlist = events.SearchEvents("holdes");
+    foreach(Events Events in eventlist) Console.WriteLine(Events);
+    Console.WriteLine();
 
-//Delete event and then showing the remaining events
-Console.WriteLine("Delete of event3 and the new EventList shown:");
-events.DeleteEvents(3);
-List<Events> allevents = events.GetAllEvents();
-foreach (Events events1 in allevents) Console.WriteLine(events1);
-Console.WriteLine();
+    //Delete event and then showing the remaining events
+    Console.WriteLine("Delete of event3 and the new EventList shown:");
+    events.DeleteEvents(3);
+    List<Events> allevents = events.GetAllEvents();
+    foreach (Events events1 in allevents) Console.WriteLine(events1);
+    Console.WriteLine();
 
-//Member joins an event.
-Console.WriteLine("Member joins event1:");
-List<Events> joinEvent = new List<Events>();
-event1.JoinEvent(member);
-Console.WriteLine(events.GetEvents(1));
-Console.WriteLine(event1.ShowLastMember());
-foreach (Events joinevent1 in joinEvent) Console.WriteLine(joinevent1);
-Console.WriteLine();
+    //Member joins an event.
+    Console.WriteLine("Member joins event1:");
+    List<Events> joinEvent = new List<Events>();
+    event1.JoinEvent(member);
+    Console.WriteLine(events.GetEvents(1));
+    Console.WriteLine(event1.ShowLastMember());
+    foreach (Events joinevent1 in joinEvent) Console.WriteLine(joinevent1);
+    Console.WriteLine();
 
 Console.WriteLine(boat);
 Console.WriteLine(event1);
