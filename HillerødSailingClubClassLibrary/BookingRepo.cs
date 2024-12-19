e08e2ac52422a92244309e1e0d4500d2f8efdff5
AgentBlank66 <watchandlearn626@gmail.com>
2024-12-19 13:41:27 +0100

    Changed name of list from BookedBoats to Bookings

--- HillerødSailingClubClassLibrary/BookingRepo.cs
+++ HillerødSailingClubClassLibrary/BookingRepo.cs
@@ -10,29 +10,29 @@ namespace HillerødSailingClubClassLibrary
     public class BookingRepo
     {
         #region Properties
-        List<Booking> BookedBoats = new List<Booking>();
+        List<Booking> Bookings = new List<Booking>();
         #endregion
 
         #region Methods
         //This method adds the a booking of a boat with to a list of all bookedboats
         public void BookBoat(Booking booking)
         {
-            BookedBoats.Add(booking);
+            Bookings.Add(booking);
         }
 
         //This method removes a bookedboat from the Bookedboat list
         public void RemoveBookedBoat(Booking booking)
         {
-            if (BookedBoats.Contains(booking))
+            if (Bookings.Contains(booking))
             {
-                BookedBoats.Remove(booking);
+                Bookings.Remove(booking);
             }
         }
 
         //This method searches the BookedBoat list after a booking with a matching id
         public Booking? GetBookedBoat(int id)
         {
-            foreach (Booking booking in BookedBoats)
+            foreach (Booking booking in Bookings)
             {
                 if (booking.Id.Equals(id))
                 {
@@ -42,10 +42,10 @@ namespace HillerødSailingClubClassLibrary
             return null;
         }
 
-        // This method takes a booking object and checks if BookedBoats list contains a object with a matching Id, then overrids the old information with the new one
+        // This method takes a booking object and checks if Bookings list contains a object with a matching Id, then overrids the old information with the new one
         public void UpdateBookedBoat(Booking booking, Member newMember, Boat newBoat, int newYear, int newMonth, int newDay)
         {
-            if (BookedBoats.Contains(booking)) 
+            if (Bookings.Contains(booking)) 
             {
                 booking.Boat = newBoat;
                 booking.Member = newMember;
@@ -53,18 +53,18 @@ namespace HillerødSailingClubClassLibrary
             }
         }
 
-        // This method goes through the BookedBoats list and printout all booking objects
+        // This method goes through the Bookings list and printout all booking objects
         public List<Booking> PrintAllBookedBoats()
         {
-            return BookedBoats;
+            return Bookings;
         }
 
-        /* This method goes through all booking objects in the BookedBoats dictionary and checking
+        /* This method goes through all booking objects in the Bookings dictionary and checking
         whether the current time is later or equal to booking time plus 2 hours and 30 minutes.
         If true then we start the search, if the loope never returns anything another thing is returned */
         public string RescueMember()
         {
-            foreach (Booking booking1 in BookedBoats)
+            foreach (Booking booking1 in Bookings)
             {
                 if (DateTime.Now >= booking1.DateTime.AddHours(2).AddMinutes(30))
                 {
@@ -74,12 +74,12 @@ namespace HillerødSailingClubClassLibrary
             return "Medlem er kommet retur";
         }
 
-        /* This method starts by creating a new list, then goes through all objects in BookedBoats
+        /* This method starts by creating a new list, then goes through all objects in Bookings
          dictionary, takes the Member objects and adds them to the new list and returns that list */
         public List<Member> MembersSailing()
         {
             List<Member> membersSailing = new List<Member>();
-            foreach(Booking booking in BookedBoats)
+            foreach(Booking booking in Bookings)
             {
                 membersSailing.Add(booking.Member);
             }
